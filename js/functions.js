ed6fd5819548a2b6f8cd73bfff08e4332cb2e8c6
luciatorralboordonez <59249819+luciatorralboordonez@users.noreply.github.com>
2019-12-27 13:15:00 +0100

    ideas de moda 2020

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-var time= new Date();
+/*var time= new Date();
 var color;
 var myElement;
 
@@ -12,3 +12,14 @@ else {
     color="blue";
 }
 document.getElementById("timecolor").innerHTML=color;
+*/function alerta()
+{
+    var mensaje;
+    var opcion = confirm("¿Te gustaria suscribirte a nuestra web?");
+    if (opcion == true) {
+        mensaje = "Te enviaremos nuestra revista todos los meses";
+	} else {
+	    mensaje = "Lo sentimos, intentamos hacerlo lo mejor posible";
+	}
+	document.getElementById("ejemplo").innerHTML = mensaje;
+}
